53121556e5c60cc745a402db1bef9ccb834ef8d5
Thomas Goorden <tg@eenwereldmetlef.be>
2019-01-07 18:29:16 +0100

    Added sass plugin and succesfully compiled site

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,25 @@
+const sass = require('node-sass');
+const fs = require('fs');
 module.exports = function(eleventyConfig) {
   // Filter source file names using a glob
   eleventyConfig.addCollection("posts", function(collection) {
     return collection.getFilteredByGlob("_posts/*.md");
   });
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("css");
+  //eleventyConfig.addPassthroughCopy("css");
+  sass.render({
+    file: "css/main.scss"
+  }, function(err,result) {
+    if (!err) {
+      fs.writeFile("_site/css/main.css", result.css, function(error){
+        if(error){
+          console.log(error);
+        }
+      });
+    } else {
+      console.log(err);
+    }
+  });
   return {
     passthroughFileCopy: true
   };
