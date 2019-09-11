21a523fcb58ceb90d715019546c52b575b92dc4a
laerts <la@eenwereldmetlef.be>
2019-09-11 16:06:34 +0200

    Basic template and data + index page

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,15 @@ module.exports = async function (eleventyConfig) {
     return md.render(content)
   })
 
+  eleventyConfig.addCollection('sortedPosts', function (blogposts) {
+    blogposts = require('./_data/blogposts.json')
+    blogposts.sort(function (a, b) {
+      if (a.date < b.date) return 1
+      if (a.date > b.date) return -1
+    })
+    return blogposts
+  })
+
   eleventyConfig.addPassthroughCopy('assets')
 
   return {
